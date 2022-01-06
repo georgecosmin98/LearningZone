d97f162984ea0a1ba91328cbb43bc51592f7cc73
Ilinca George-Cosmin <ilincageorgecosmin@yahoo.com>
2022-01-06 13:46:42 +0200

    Data Generators implementation from Thinking In Java Book :sunglasses:

---
+++ ThinkingInJava/src/Arrays/DataGenerators/Generator.java
@@ -0,0 +1,5 @@
+package Arrays.DataGenerators;
+
+public interface Generator<T> {
+    T next();
+}
---
+++ ThinkingInJava/src/Arrays/DataGenerators/RandomGeneratorTest.java
@@ -0,0 +1,7 @@
+package Arrays.DataGenerators;
+
+public class RandomGeneratorTest {
+    public static void main(String[] args) {
+        GeneratorTest.test(RandomGenerator.class);
+    }
+}
